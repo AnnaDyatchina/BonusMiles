7565cf9591fad51c9fc7df7ba985ba84e183d050
Anna <zoloto86@list.ru>
2022-11-14 11:39:11 +0700

    addcommit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 10_000;
+        int miles = 20 * 1;
+        int bonusMiles = ticket / miles;
+
+        System.out.println(bonusMiles + " бонусных миль");
+
+    }
+}
+
